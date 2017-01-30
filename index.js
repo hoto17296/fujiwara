b7b17060c486bdc7a46699fb661a25e6a8032d63
石川 雄基 / ほと <yuuki.ishikawa@gaiax.com>
2017-01-30 16:20:11 +0900

    add ゛ for each character

---
+++ index.js
@@ -0,0 +1,9 @@
+module.exports = function(text, opts) {
+  opts = Object.assign({
+    separator: '゛',
+  }, opts);
+  if ( typeof text.split !== 'function' ) throw new TypeError();
+  return text.split().map((char) => {
+    return char + opts.separator;
+  }).join();
+}
--- test/index.js
+++ test/index.js
@@ -1,2 +1,14 @@
 var fujiwara = require('..');
 var assert = require('assert');
+
+describe('fujiwara', () => {
+
+  context('input string', () => {
+    it('should add ゛ for each character', () => {
+      var text = 'ああああああああ！！！';
+      var expect = 'あ゛あ゛あ゛あ゛あ゛あ゛あ゛あ゛！゛！゛！゛';
+      assert(fujiwara(text), expect);
+    });
+  });
+
+});
