e0a639e582cc35a73093902ea88549ebade28699
sota1235 <sota1235@gmail.com>
2017-01-31 14:12:02 +0900

    modify: use strict and change the way to check type of the argument

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
+"use strict";
+
 const FujiwaraError = require('./fujiwara-error');
+const isString = require('lodash.isstring');
 
 module.exports = function(text, opts) {
-  try {
-    opts = Object.assign({
-      separator: '゛',
-      ignoreSpaces: true,
-      ignoreChars: [],
-    }, opts);
-    if ( typeof text.split !== 'function' ) throw new TypeError();
-    if ( opts.ignoreSpaces ) {
-      opts.ignoreChars = opts.ignoreChars.concat([' ', '　']);
-    }
-    return text.split('').map((char) => {
-      if ( char.charCodeAt() < 0x20 ) return char;
-      if ( opts.ignoreChars.indexOf(char) !== -1 ) return char;
-      return char + opts.separator;
-    }).join('');
-  } catch (err) {
+  if (!isString(text)) {
     const errorMessage = 'そ゛ん゛な゛ひ゛き゛す゛う゛わ゛か゛ら゛ね゛え゛よ゛お゛お゛お゛お゛！゛！゛！゛';
     throw new FujiwaraError(errorMessage);
   }
+
+  opts = Object.assign({
+    separator: '゛',
+    ignoreSpaces: true,
+    ignoreChars: [],
+  }, opts);
+  if ( opts.ignoreSpaces ) {
+    opts.ignoreChars = opts.ignoreChars.concat([' ', '　']);
+  }
+  return text.split('').map((char) => {
+    if ( char.charCodeAt() < 0x20 ) return char;
+    if ( opts.ignoreChars.indexOf(char) !== -1 ) return char;
+    return char + opts.separator;
+  }).join('');
 }
